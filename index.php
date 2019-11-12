f5489df16d88b589129c7e3ce83fdf21db991d1e
Senathor <57641561+Senathor@users.noreply.github.com>
2019-11-12 23:20:17 +0100

    Add files via upload
    Strona glowna - Konrad Sokolowski

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+include "./views/index.php";
+include "./views/api.php";
+
+// Publication::get_publications_list("14");
+// Publication::add_author("14", ["73", "74"]);
+// Publication::delete_publication('32');
+// Publication::update_publication("14", [
+//     "points" => 50,
+//     "magazine" => "Wprost"
+// ]);
+// echo Publication::save_publication(
+//     "Wesołe miasteczko w Skierniewicah",
+//     json_encode([
+//         "Jack",
+//         "Mark"
+//     ]),
+//     json_encode([
+//         "Jack" => 80,
+//         "Mark" => 20
+//     ]),
+//     50,
+//     "NewsWeek",
+//     "https://google.com.pl.eu.net.com"
+// );
+// User::update("73", [
+//     "name" => "Jack",
+//     "login" => "new_logins"
+// ]);
+
+// echo joins(" = ")([
+//     "name" => "Jack",
+//     "login" => "new_login"
+// ]);
